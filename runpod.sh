2a563505fbb52daaf2a76bd9a65db2dd8f307008
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-08-14 11:34:54 +0530

    Create runpod.sh

---
+++ runpod.sh
@@ -0,0 +1,12 @@
+# download and install 
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+chmod +x Miniforge3-$(uname)-$(uname -m).sh
+bash Miniforge3-$(uname)-$(uname -m).sh -b
+
+# initiate mamba
+${HOME}/miniforge3/mamba init
+source ${HOME}/.bashrc
+
+# configure mamba
+conda config --set auto_activate_base false
+mamba update -n base --all
